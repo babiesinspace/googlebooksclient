2fd2270e7af9954f4df4d4b4d2178337c9faddb0
Alex Cooper <allie.g.cooper@gmail.com>
2018-11-13 15:00:35 -0500

    requests responding with books, turns out no api key is needed for this version

--- lib/googlebooksclient/client.rb
+++ lib/googlebooksclient/client.rb
@@ -6,6 +6,11 @@ module Googlebooksclient
       @access_token = access_token || ENV["GOOGLEBOOKS_ACCESS_TOKEN"]
     end
 
+    def perform_request(path)
+      url = "https://www.googleapis.com/books/v1/volumes?q=#{path}"
+      response = HTTParty.get(url)
+      JSON.parse(response.body)
+    end
   end
 
 end
