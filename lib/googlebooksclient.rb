75caf2367733dfa872f373542e6035a2a133ee75
Alex Cooper <allie.g.cooper@gmail.com>
2018-11-13 13:18:20 -0500

    initial commit

---
+++ lib/googlebooksclient.rb
@@ -0,0 +1,6 @@
+require "googlebooksclient/version"
+
+module Googlebooksclient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/googlebooksclient/version.rb
@@ -0,0 +1,3 @@
+module Googlebooksclient
+  VERSION = "0.1.0"
+end
