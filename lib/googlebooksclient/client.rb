25a86a958f7babc78d1843d4c53368bafea9fd25
Alex Cooper <allie.g.cooper@gmail.com>
2018-11-14 14:39:13 -0500

    seperate volume search into module so each class method can be responsible for a specific request. Add logic to prevent error if no api key given to google for simple searches.

--- lib/googlebooksclient/client.rb
+++ lib/googlebooksclient/client.rb
@@ -1,16 +1,21 @@
+require "httparty"
+require "googlebooksclient/client/volumes"
+
 module Googlebooksclient
 
   class Client
+    include HTTParty
+    include Googlebooksclient::Client::Volumes
+    base_uri "https://www.googleapis.com/books/v1"
+    format :json
 
     def initialize(access_token = nil)
-      @access_token = access_token || ENV["GOOGLEBOOKS_ACCESS_TOKEN"]
+      access_token ||= ENV["GOOGLEBOOKS_ACCESS_TOKEN"]
+      if !access_token.nil?
+        self.class.default_options.merge!(headers: { 'Authorization' => "Bearer #{access_token}" })
+      end
     end
 
-    def perform_request(path)
-      url = "https://www.googleapis.com/books/v1/volumes?q=#{path}"
-      response = HTTParty.get(url)
-      JSON.parse(response.body)
-    end
   end
 
 end
